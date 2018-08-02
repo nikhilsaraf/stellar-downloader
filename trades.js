ea902c273cd7efc52f2900d2d6687c42f41df64c
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2018-08-02 11:31:42 -0700

    separate out functions to write history and print to display

--- trades.js
+++ trades.js
@@ -1,7 +1,7 @@
 var stellar = require('stellar-sdk')
 
 if (process.argv.length <= 2) {
-    console.log("need to pass in the account as a command line argument");
+    printDisplay("need to pass in the account as a command line argument");
     process.exit()
 }
 
@@ -24,13 +24,21 @@ var to_csv = function(arr) {
     return s;
 }
 
+var printDisplay = function(str) {
+    console.error(str);
+}
+
+var writeHistory = function(str) {
+    console.log(str);
+}
+
 var register = function(a) {
     if (!(a in asset_map)) {
         asset_map[a] = 1;
         asset_list.push(a);
         header.push(a);
-        console.error();
-        console.error(to_csv(header));
+        printDisplay("");
+        printDisplay(to_csv(header));
     }
 }
 
@@ -38,7 +46,7 @@ var tradesPrinter = function(t) {
     var last_cursor = "";
     t.records.forEach(function(r) {
         if (r.type == 'account_created') {
-            console.log(to_csv(['starting_balance', r.paging_token, r.created_at, 'XLM', r.starting_balance,null,null, r.account]));
+            writeHistory(to_csv(['starting_balance', r.paging_token, r.created_at, 'XLM', r.starting_balance,null,null, r.account]));
         }
 
         if (r.type != 'trade') {
@@ -61,7 +69,7 @@ var tradesPrinter = function(t) {
                 line.push(null);
             }
         }
-        console.log(to_csv(line));
+        writeHistory(to_csv(line));
         last_cursor = r.paging_token;
     });
 
@@ -77,8 +85,8 @@ var tradesPrinter = function(t) {
 }
 
 server.loadAccount(account).then(function(a) {
-    console.log('Trades for account: ' + account);
-    console.log(to_csv(header));
+    writeHistory('Trades for account: ' + account);
+    writeHistory(to_csv(header));
     server.effects()
         .forAccount(account)
         .order("asc")
