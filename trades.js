52992fd28765b02055d2d2e1cc65f75e9eff1daf
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2018-08-02 20:38:25 -0700

    added cumulative balance and interleaving along with some TODOs

--- trades.js
+++ trades.js
@@ -9,9 +9,9 @@ var account = process.argv[2]
 var server = new stellar.Server('https://horizon.stellar.org');
 var limit = 200;
 
-var header = ['type','paging_token','date','bought_asset','bought_amount','sell_asset','sell_amount','counterparty_account']
-var asset_map = {};
-var asset_list = [];
+var header = ['type','paging_token','date','bought_asset','bought_amount','sell_asset','sell_amount','counterparty_account','XLM']
+var asset_map = {'XLM' : 0};
+var asset_list = ['XLM'];
 // payment, account_created, and account_merge type payments keyed by paging_token
 var payments = {};
 var last_payment_cursor = "";
@@ -35,9 +35,9 @@ var writeHistory = function(str) {
     console.log(str);
 }
 
-var register = function(a) {
+var register = function(a, start_amount) {
     if (!(a in asset_map)) {
-        asset_map[a] = 1;
+        asset_map[a] = start_amount;
         asset_list.push(a);
         header.push(a);
         printDisplay("");
@@ -45,6 +45,7 @@ var register = function(a) {
     }
 }
 
+// TODO need to have this run one step ahead of the tradesPrinter
 var paymentsHandler = function(t) {
     var last_cursor = "";
     t.records.forEach(function(r) {
@@ -68,8 +69,7 @@ var paymentsHandler = function(t) {
             payments[r.paging_token] = {
                 type: r.type,
                 account: r.account,
-                into: r.into,
-                amount: r.amount
+                into: r.into
             };
         }
         last_cursor = r.paging_token;
@@ -87,35 +87,67 @@ var paymentsHandler = function(t) {
     }
 }
 
+var appendAssets = function(list) {
+    for (i = 0; i < asset_list.length; i++) {
+        value = asset_map[asset_list[i]]
+        list.push(value);
+    }
+}
+
+var mapData = function(effect_paging_token) {
+    payment_pt = effect_paging_token.split("-")[0];
+    return payments[payment_pt];
+}
+
 var tradesPrinter = function(t) {
     var last_cursor = "";
     t.records.forEach(function(r) {
+        last_cursor = r.paging_token;
+        var line = [];
+        // TODO need to check if m_data is available or not in all these cases, sometimes the corresponding operation may not exist on the current account so we will need to fetch the operation on the effect
         if (r.type == 'account_created') {
-            writeHistory(to_csv(['starting_balance', r.paging_token, r.created_at, 'XLM', r.starting_balance,null,null, r.account]));
-        }
-
-        if (r.type != 'trade') {
-            last_cursor = r.paging_token;
+            // only applies to the current account being created
+            m_data = mapData(r.paging_token);
+            line = ['genesis', r.paging_token, r.created_at, 'XLM', r.starting_balance, null, null, m_data['funder']];
+            asset_map['XLM'] += r.starting_balance;
+        } else if (r.type == 'account_debited') {
+            m_data = mapData(r.paging_token);
+            if (m_data['type'] == 'payment') {
+                line = ['payment_sent', r.paging_token, r.created_at, null, null, m_data['asset'], r.amount, m_data['to']];
+                asset_map[m_data['asset']] -= r.amount;
+            } else if (m_data['type'] == 'create_account') {
+                line = ['account_created', r.paging_token, r.created_at, null, null, 'XLM', m_data['starting_balance'], m_data['account']];
+                asset_map['XLM'] -= m_data['starting_balance'];
+                // TODO need to spider this account
+            } else if (m_data['type'] == 'account_merge') {
+                line = ['payment_sent', r.paging_token, r.created_at, null, null, 'XLM', r.amount, m_data['into']];
+                asset_map['XLM'] -= r.amount;
+            } 
+        } else if (r.type == 'account_credited') {
+            m_data = mapData(r.paging_token);
+            if (m_data['type'] == 'payment') {
+                line = ['payment_received', r.paging_token, r.created_at, m_data['asset'], r.amount, null, null, m_data['from']];
+                asset_map[m_data['asset']] += r.amount;
+            } else if (m_data['type'] == 'account_merge') {
+                line = ['payment_received', r.paging_token, r.created_at, 'XLM', r.amount, null, null, m_data['account']];
+                asset_map['XLM'] += r.amount;
+            }
+            // TODO raise
+            // it is never the case that effect is account_credited and operation is created_account (that's why we have the account_created effect)
+        } else if (r.type == 'trade') {
+            bought_asset = r.bought_asset_type == 'native' ? 'XLM' : r.bought_asset_code + ":" + r.bought_asset_issuer;
+            sold_asset = r.sold_asset_type == 'native' ? 'XLM' : r.sold_asset_code + ":" + r.sold_asset_issuer;
+            line = ['trade', r.paging_token, r.created_at, bought_asset, r.bought_amount, sold_asset, r.sold_amount, r.seller];
+            register(bought_asset, r.bought_amount);
+            asset_map[sold_asset] -= r.sold_amount;
+        } else if (r.type == 'account_removed') {
+            line = ['end', r.paging_token, r.created_at, null, null, null, null, null];
+        } else {
             return;
         }
 
-        bought_asset = r.bought_asset_type == 'native' ? 'XLM' : r.bought_asset_code + ":" + r.bought_asset_issuer;
-        sold_asset = r.sold_asset_type == 'native' ? 'XLM' : r.sold_asset_code + ":" + r.sold_asset_issuer;
-        register(bought_asset)
-        register(sold_asset)
-
-        var line = ['trade', r.paging_token, r.created_at, bought_asset, r.bought_amount, sold_asset, r.sold_amount, r.seller];
-        for (i = 0; i < asset_list.length; i++) {
-            if (bought_asset == asset_list[i]) {
-                line.push(r.bought_amount);
-            } else if (sold_asset == asset_list[i]) {
-                line.push(-r.sold_amount);
-            } else {
-                line.push(null);
-            }
-        }
+        appendAssets(line);
         writeHistory(to_csv(line));
-        last_cursor = r.paging_token;
     });
 
     if (last_cursor != '') {
