4fd65294ae4844c862fea5e4d446a756eeb3c588
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2018-09-03 14:04:45 -0700

    consolidateAccountsCsv.sh

---
+++ consolidateAccountsCsv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# script to consolidate the accounts csv files passed in as parameters into a single csv file representing the full portfolio
+
+if [[ $# -lt 1 ]]
+then
+    echo "Usage: $0 account [account ...]"
+    exit 1
+fi
+
+OUTPUT=portfolio.csv
+echo -n "" > $OUTPUT
+OUTPUT_TEMP=portfolio-temp.csv
+
+for ACCOUNT in $@
+do
+    if [[ $ACCOUNT == *.* ]]; then
+        echo "skipping account $ACCOUNT, only use account, not filename"
+        continue
+    fi
+
+    echo "appending data for account $ACCOUNT"
+    FILE=$ACCOUNT.csv
+    tail -n +2 $FILE | cut -d',' -f2,3,9,10 >> $OUTPUT
+    echo "    ... finished appending data for account $ACCOUNT"
+    echo ""
+done
+
+echo "consolidating files ..."
+echo "paging_token,date,portfolio_value_xlm,portfolio_value_usd" > $OUTPUT_TEMP
+cat $OUTPUT | sort -k1 -t, >> $OUTPUT_TEMP
+mv $OUTPUT_TEMP $OUTPUT
+echo "   ... finished consolidating files"
+echo "done: $OUTPUT"
